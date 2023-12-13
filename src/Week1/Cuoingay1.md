25f30ec372871400753fb3bdfd14c62a9feb7bf4
Dino? <daophuduytan2203@gmail.com>
2023-12-14 00:53:02 +0700

    Bài 1 và bài 2 trong chuỗi seri 300 bài code thiếu nhi

---
+++ src/Week1/Cuoingay1.md
@@ -0,0 +1,9 @@
+
+## Thu hoạch ngày 1
+### Bài 1 
+
+làm quen được với `hashmap` đồng thời ôn tập lai *cách khai báo mãng*
+biết được thêm thuật ngữ **Giải pháp trâu bò**
+### Bài 2
+làm quen với cách sử dụng `Stack` trong thực triến 
+đồng thời ôn tập lại *cách khai báo chuỗi* 
---
+++ src/Week1/Leetcode_1.java
@@ -0,0 +1,33 @@
+package Week1;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leetcode_1 {
+    //Giai phap trau bo
+    public int[] twoSum (int[] nums , int target )
+    {
+        for ( int i = 0 ; i < nums.length ; i++)
+            for ( int j = i +1; j <nums.length ;j++ )
+                if( nums[j] == target- nums[i] )
+                {
+                    return new int[] {i , j };
+                }
+
+        throw new IllegalArgumentException("Khong co ket qua");
+    }
+    //Giai phap voi mãng băm
+    public int[] twoSum2 (int[] arr , int target)
+    {
+        Map<Integer , Integer> map = new HashMap<>() ;
+        for ( int i = 0 ; i < arr.length ; i ++)
+        {
+            int soKhac = arr[i] - target ;
+            //Duyet qua hashmap xem co gia tri "soKhac" ton tai hay chua
+            if (map.containsKey(soKhac))
+                return new int[] {map.get(soKhac) , i} ;
+            //day no vao hasmap
+            map.put(arr[i] , i ) ;
+        }
+        throw new IllegalArgumentException("Khong co ket qua ") ;
+    }
+}
