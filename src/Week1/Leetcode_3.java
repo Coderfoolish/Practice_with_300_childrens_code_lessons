a5f11b6bcf18d1e8a87fedfe80a695eb933448c2
Dino? <daophuduytan2203@gmail.com>
2023-12-14 22:42:36 +0700

    baitapnggaythu2

---
+++ src/Week1/Leetcode_3.java
@@ -0,0 +1,32 @@
+package Week1;
+
+public class Leetcode_3 {
+    class ListNode {
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        int val;
+        ListNode next;
+    }
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode tail = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = l1 == null ? l2 : l1;
+
+        return head.next;
+    }
+}
